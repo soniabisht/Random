986d919cf8c5ad4528408c8927baa2c3dc6fc4dc
soniabisht <47450482+soniabisht@users.noreply.github.com>
2019-11-10 08:34:38 +0530

    Read a continuously changing file lines

---
+++ realTimeFileReading.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+def follow(name):
+    current = open(name, "r")
+    curino = os.fstat(current.fileno()).st_ino
+    while True:
+        while True:
+            line = current.readline()
+            if not line:
+                break
+            yield line
+
+        try:
+            if os.stat(name).st_ino != curino:
+                new = open(name, "r")
+                current.close()
+                current = new
+                curino = os.fstat(current.fileno()).st_ino
+                continue
+        except IOError:
+            pass
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    fname = "test.log"
+    for l in follow(fname):
+        print("LINE: {}".format(l))
